93585f061021a17c3588096dbedeee581511797e
topjeans6 <topjeans6@gmail.com>
2026-04-13 21:53:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -624,15 +624,6 @@ def admin_users():
     users = User.query.all()
     return render_template("admin_users.html", users=users)
 
-@app.route("/admin/clear-test-sales")
-@login_required
-def clear_test_sales():
-    if current_user.role != "admin":
-        return "เฉพาะ Admin เท่านั้น"
-    SaleLog.query.filter(SaleLog.sku == "TJ25040011").delete()
-    db.session.commit()
-    return "✅ ลบเรียบร้อยครับ"
-  
 def init_db():
     with app.app_context():
         db.create_all()
